f959f1d58104b440868b1dff94b53feb00603e10
amalm2002 <amalm2495@gmail.com>
2025-09-13 15:06:38 +0530

    refactor : update the port side on server page

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import App from "./app";
 import 'dotenv/config'
 
-const port=Number(process.env.API_GATEWAY_PORT)
+const port = Number(process.env.API_GATEWAY_PORT) || 5050
 
-const app=new App()
+const app = new App()
 
 app.startServer(port)
