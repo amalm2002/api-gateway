b5daafaca257ec00d7bfdf65344d6eb4e74efc6b
amalm2002 <amalm2495@gmail.com>
2025-09-14 11:48:06 +0530

    refactor : add one console on the user client side

--- src/modules/user/config/user.client.ts
+++ src/modules/user/config/user.client.ts
@@ -6,6 +6,7 @@ import 'dotenv/config'
 const packageDef=protoLoader.loadSync(path.resolve(__dirname,'./user.proto'))
 const grpcObject=(grpc.loadPackageDefinition(packageDef)as unknown)as any
 const DOMAIN=process.env.NODE_ENV==='dev'? process.env.DEV_DOMAIN:process.env.PRO_DOMAIN_USER
+console.log(`${DOMAIN}:${process.env.USER_GRPC_PORT}`)
 const UserService=new grpcObject.user_package.UserService(
     `${DOMAIN}:${process.env.USER_GRPC_PORT}`,grpc.credentials.createInsecure()
 )
